ed9166b02fa3cd5a6b16346b0dba8682163ec26a
Subhasis <palsubhasis100@gmail.com>
2023-05-05 16:17:24 +0530

    Initial commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {getAllUsers,registerController,loginController} = require("../controllers/userControllers");
+
+//router object
+const router = express.Router();
+
+// GET ALL USERS || GET
+router.get("/all-users", getAllUsers);
+
+// CREATE USER || POST
+router.post("/register", registerController);
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+module.exports = router;
